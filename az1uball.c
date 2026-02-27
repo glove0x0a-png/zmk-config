a47df7ad729d93b57a10e1c6f6e8c9f7d49be77c
glove0x0a-png <glove0x0a@gmail.com>
2026-02-27 22:43:46 +0900

    20260227_2243_releace

--- az1uball.c
+++ az1uball.c
@@ -103,10 +103,10 @@ void az1uball_read_data_work(struct k_work *work)
 
     int16_t delta_x,delta_y;
     //移動距離(誤作動防止)
-    if( abs((int16_t)buf[1])   > abs(buf[0])+5) delta_x= 50//delta_x =    (int16_t)buf[1] * 6;
-    if( abs((int16_t)buf[1])+5 < abs(buf[0])  ) delta_x=-50//delta_x = -1*(int16_t)buf[0] * 6;
-    if( abs((int16_t)buf[3])   > abs(buf[2])+5) delta_y= 50//delta_y =    (int16_t)buf[3] * 6;
-    if( abs((int16_t)buf[3])+5 < abs(buf[2])  ) delta_y=-50//delta_y = -1*(int16_t)buf[2] * 6;
+    if( abs((int16_t)buf[1])   > abs(buf[0])+5) delta_x= 50; //delta_x =    (int16_t)buf[1] * 6;
+    if( abs((int16_t)buf[1])+5 < abs(buf[0])  ) delta_x=-50; //delta_x = -1*(int16_t)buf[0] * 6;
+    if( abs((int16_t)buf[3])   > abs(buf[2])+5) delta_y= 50; //delta_y =    (int16_t)buf[3] * 6;
+    if( abs((int16_t)buf[3])+5 < abs(buf[2])  ) delta_y=-50; //delta_y = -1*(int16_t)buf[2] * 6;
     bool  btn_push  = (buf[4] & MSK_SWITCH_STATE) != 0;
 
     //現レイヤ
