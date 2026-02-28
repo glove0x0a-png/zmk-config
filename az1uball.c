e58ef5cfde39d5b232d2eb8a004d0f25d4e44f48
glove0x0a-png <glove0x0a@gmail.com>
2026-02-28 12:56:41 +0900

    20260228_1256_releace

--- az1uball.c
+++ az1uball.c
@@ -25,9 +25,9 @@
 #define JIGGLE_DELTA_X 1                      // X方向にnピクセル分動かす
 
 #define DED_ZONE 0                            // デッドゾーン/マウス動作を検知するまで
-#define MOUSE_VAL 6                           // マウス移動量
+#define MOUSE_VAL 3                           // マウス移動量
 #define NUTORAL 500                           // 前回移動量の無効化時間(ms)
-#define ACCEL 2                               // 加速度
+#define ACCEL 1                               // 加速度
 
 
 //struct
