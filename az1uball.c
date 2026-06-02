0f0dbddffb931ca3171e4a508042b6b3b8026374
glove0x0a-png <glove0x0a@gmail.com>
2026-06-02 23:56:58 +0900

    20260602_2356_releace

--- az1uball.c
+++ az1uball.c
@@ -237,6 +237,7 @@ static void az1uball_set_poll_mode(struct az1uball_data *data, uint8_t mode)
 void zmk_sleep(void)
 {
     struct az1uball_data *data = &az1uball_data_0;
+    if (data->jiggler_on) {
         /* ジグラー ON → 超低頻度ポーリング（4分） */
         az1uball_set_poll_mode(data, POLL_MODE_JIG);
     } else {
